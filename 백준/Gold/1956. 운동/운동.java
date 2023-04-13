6eef1548c39577e77ec584f67c10113c4f7b71d3
Endura <dhrtnqls0535@naver.com>
2023-04-13 10:04:51 +0900

    [Gold IV] Title: 운동, Time: 468 ms, Memory: 58144 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -22,10 +22,10 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			if(a != b)
-				map[a - 1][b - 1] = Math.min(map[a - 1][b - 1], c);
+			map[a - 1][b - 1] = Math.min(map[a - 1][b - 1], c);
 		}
 
+		// 플로이드-워셜
 		for (int w = 0; w < V; w++) {
 			for (int s = 0; s < V; s++) {
 				for (int e = 0; e < V; e++) {
@@ -36,10 +36,12 @@ public class Main {
 
 		int answer = Integer.MAX_VALUE;
 		
+		// 자기 자신으로 돌아오는 경로의 비용 중 최소 값 탐색
 		for (int i = 0; i < V-1; i++) {
 			answer = Math.min(answer, map[i][i]);
 		}
-		if(answer == 4000001)
+		
+		if(answer == 4000001)				// 갈 수 없으면 -1 출력
 			System.out.println(-1);
 		else
 			System.out.println(answer);
