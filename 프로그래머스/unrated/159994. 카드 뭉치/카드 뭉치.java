6835a5ab8b047d8d2e4c39d13ad3476cb6bc5c58
Endura <dhrtnqls0535@naver.com>
2023-07-12 10:01:37 +0900

    [unrated] Title: 카드 뭉치, Time: 0.13 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> q1 = new LinkedList<>();
+        Queue<String> q2 = new LinkedList<>();
+        
+        for(String s : cards1){
+            q1.offer(s);
+        }        
+        
+        for(String s : cards2){
+            q2.offer(s);
+        }
+        
+        for(String s : goal){
+            if(s.equals(q1.peek())){
+                q1.poll();
+            }else if(s.equals(q2.peek())){
+                q2.poll();
+            }else{
+                return "No";
+            }
+        }
+        
+        return "Yes";
+    }
+}
