31377bb1b9ff562a0f6e85a3fbf7649cceea4e49
So_ob <dhrtnqls0535@naver.com>
2024-07-29 23:28:16 +0900

    [level 2] Title: 교점에 별 만들기, Time: 37.10 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,66 +1,10 @@
-import java.util.*;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-class Solution {
-    public String[] solution(int[][] line) {
-        List<long[]> list = new ArrayList<>();
-
-        long xMin = Long.MAX_VALUE;
-        long yMin = Long.MAX_VALUE;
-        long xMax = Long.MIN_VALUE;
-        long yMax = Long.MIN_VALUE;
-
-        for (int i = 0; i < line.length - 1; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                long[] intersection = 
-                    intersect(line[i][0], line[i][1], line[j][0], line[j][1], line[i][2], line[j][2]);
-                if (intersection[0] == 1001 || intersection[1] == 1001)
-                    continue;
-                xMin = Math.min(xMin, intersection[0]);
-                xMax = Math.max(xMax, intersection[0]);
-                yMin = Math.min(yMin, intersection[1]);
-                yMax = Math.max(yMax, intersection[1]);
-
-                list.add(intersection);
-            }
-        }
-
-        int width = (int) (xMax - xMin + 1);
-        int height = (int) (yMax - yMin + 1);
-        char[][] arr = new char[height][width];
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                arr[y][x] = '.';
-            }
-        }
-
-        for (long[] l : list) {
-            int y = (int) (l[1] - yMin);
-            int x = (int) (l[0] - xMin);
-            arr[y][x] = '*';
-        }
-
-        String[] answer = new String[height];
-        for (int y = 0; y < height; y++) {
-            answer[y] = new String(arr[height - 1 - y]);
-        }
-
-        return answer;
-    }
-    
-    public long[] intersect(int a, int b, int c, int d, int e, int f){
-        long denominator = (long) a * d - (long) b * c;
-        if (denominator == 0)
-            return new long[]{1001, 1001};
-
-        long bfed = (long) b * f - (long) e * d;
-        long ecaf = (long) e * c - (long) a * f;
-
-        if (bfed % denominator == 0 && ecaf % denominator == 0) {
-            long x = bfed / denominator;
-            long y = ecaf / denominator;
-            return new long[]{x, y};
-        }
-        return new long[]{1001, 1001};
-    }
+// line_rows는 2차원 배열 line의 행 길이, line_cols는 2차원 배열 line의 열 길이입니다.
+char** solution(int** line, size_t line_rows, size_t line_cols) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char** answer = (char**)malloc(1);
+    return answer;
 }
