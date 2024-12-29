0f84ca8a2237e89db2939c77a6ee24a2c9adb53d
So_ob <dhrtnqls0535@naver.com>
2024-12-30 02:45:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 194.75 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -6,31 +6,30 @@ import java.util.Set;
 class Solution {
     public int solution(int[] topping) {
         Map<Integer, Integer> chulsuMap = new HashMap<>();
-        Set<Integer> chulsuSet = new HashSet<>();
         Set<Integer> broSet = new HashSet<>();
+
         for (int t : topping) {
-            if (chulsuMap.containsKey(t))
-                chulsuMap.put(t, chulsuMap.get(t) + 1);
-            else chulsuMap.put(t, 1);
-            chulsuSet.add(t);
+            chulsuMap.put(t, chulsuMap.getOrDefault(t, 0) + 1);
         }
 
-        int chulsuTopping = chulsuSet.size();
+        int chulsuTopping = chulsuMap.size();
         int broTopping = 0;
         int answer = 0;
-        for (int i = topping.length - 1; i >= 0; i--) {
-            int t = topping[i];
-            if (chulsuMap.get(t) == 1) {
-                chulsuSet.remove(t);
+
+        for (int t : topping) {
+            chulsuMap.put(t, chulsuMap.get(t) - 1);
+            if (chulsuMap.get(t) == 0) {
+                chulsuMap.remove(t);
                 chulsuTopping--;
             }
-            if (!broSet.contains(t)) {
-                broSet.add(t);
+
+            if (broSet.add(t)) {
                 broTopping++;
             }
-            chulsuMap.put(t, chulsuMap.get(t) - 1);
-            if (chulsuTopping == broTopping)
+
+            if (chulsuTopping == broTopping) {
                 answer++;
+            }
         }
 
         return answer;
