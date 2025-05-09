6d33bf583c0ef0b6db8bfec175137064f58d4c1b
So_ob <dhrtnqls0535@naver.com>
2025-05-10 02:58:14 +0900

    [Gold V] Title: 내일 할거야, Time: 1424 ms, Memory: 290176 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.java
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -1,25 +1,30 @@
-import java.io.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        List<Point> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            list.add(new Point(d, t));
         }
 
-        Arrays.sort(arr, (a, b) -> b[1] - a[1]);
+        list.sort((o1, o2) -> o2.y - o1.y);
 
-        int time = Integer.MAX_VALUE;
-        for (int[] task : arr) {
-            time = Math.min(time, task[1]);
-            time -= task[0];
+        int emptyMax = 1_000_000_000;
+        for (Point point : list) {
+            emptyMax = Math.min(emptyMax, point.y);
+            emptyMax -= point.x;
         }
 
-        System.out.println(time < 0 ? -1 : time);
+        System.out.println(emptyMax);
     }
 }
