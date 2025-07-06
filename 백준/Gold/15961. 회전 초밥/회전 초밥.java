3ecbcd5d012d21fdc8b808360f1b041d618ff8c5
So_ob <dhrtnqls0535@naver.com>
2025-07-07 02:40:29 +0900

    [Gold IV] Title: 회전 초밥, Time: 468 ms, Memory: 168640 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,62 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static int N, d, k, c;
+        int N = Integer.parseInt(st.nextToken()); // 접시 수
+        int d = Integer.parseInt(st.nextToken()); // 초밥 가짓수
+        int k = Integer.parseInt(st.nextToken()); // 연속 접시 수
+        int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		int[] sushi = new int[N];
-		int[] selected = new int[d + 1];
+        int[] belt = new int[N];
+        for (int i = 0; i < N; i++) {
+            belt[i] = Integer.parseInt(br.readLine());
+        }
 
-		for (int i = 0; i < N; i++) {
-			sushi[i] = Integer.parseInt(br.readLine());
-		}
+        int[] count = new int[d + 1];
+        int kind = 0;
 
-		int count = 0;
-		for (int i = 0; i < k; i++) {
-			if (selected[sushi[i]] == 0) {
-				count++;
-			}
+        for (int i = 0; i < k; i++) {
+            if (count[belt[i]]++ == 0) kind++;
+        }
 
-			selected[sushi[i]]++;
-		}
+        int max = count[c] == 0 ? kind + 1 : kind;
 
-		int answer = count;
+        for (int i = 1; i < N; i++) {
+            int remove = belt[(i - 1) % N];
+            if (--count[remove] == 0) kind--;
 
-		for (int i = 1; i < N; i++) {
+            int add = belt[(i + k - 1) % N];
+            if (count[add]++ == 0) kind++;
 
-			if (answer <= count) {
-				// 쿠폰 초밥을 안먹었으면 쿠폰 초밥을 먹을 수 있으므로 +1
-				if (selected[c] == 0) {
-					answer = count + 1;
-				} else {
-					answer = count;
-				}
-			}
+            int total = count[c] == 0 ? kind + 1 : kind;
+            max = Math.max(max, total);
+        }
 
-			int end = (i + k - 1) % N;
-			if (selected[sushi[end]] == 0) {
-				count++;
-			}
-			selected[sushi[end]]++;
-
-			selected[sushi[i - 1]]--;
-			if (selected[sushi[i - 1]] == 0) {
-				count--;
-			}
-		}
-
-		System.out.println(answer);
-
-	}
-
+        System.out.println(max);
+    }
 }
